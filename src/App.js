21bfe3fd83582d6cf87ff644a74092b5b69e9661
Javier Cohen <javiercohenn@gmail.com>
2023-03-26 14:09:08 -0300

    Solucion al BootStrap Collapse

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 // Importo mi componente itemListContainer
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+// Importo el componente de BootStrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,34 +1,28 @@
 // Importo  mi componente CartWidget que va a estar contenido dentro de NavBar
 import CartWidget from "../CartWidget/CartWidget"
+// Importo los componentes del Navbar
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 // Creo mi componente NavBar
 const NavBar = () => {
     return (
-      <nav className="navbar navbar-expand-md navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="null">
-          💻 PC Store
-        </a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <a className="nav-item nav-link" href="null">
-              Home <span className="sr-only">(current)</span>
-            </a>
-            <a className="nav-item nav-link" href="null">
-              Monitores
-            </a>
-            <a className="nav-item nav-link" href="null">
-              Teclados
-            </a>
-            <a className="nav-item nav-link" href="null">
-              Pads
-            </a>
-          </div>
-        </div>
-        <CartWidget />
-      </nav>
+      <Navbar NavBar expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">💻 PC Store</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#Monitores">Monitores</Nav.Link>
+            <Nav.Link href="#Teclados">Teclados</Nav.Link>
+            <Nav.Link href="#Pads">Pads</Nav.Link>
+          </Nav>
+          <CartWidget />
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     );
 }
 
