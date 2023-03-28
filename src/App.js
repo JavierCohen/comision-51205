312ee4ab532042c2045d206aa3d5df79110a5542
Javier Cohen <javiercohenn@gmail.com>
2023-03-28 19:25:51 -0300

    cart css update

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
         <ItemListContainer greeting={'Bienvenidos'}/>
       </div>
     </div>
-
   );
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,21 +8,21 @@ import Navbar from 'react-bootstrap/Navbar';
 // Creo mi componente NavBar
 const NavBar = () => {
     return (
-      <Navbar NavBar expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="#home">💻 PC Store</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#Monitores">Monitores</Nav.Link>
-            <Nav.Link href="#Teclados">Teclados</Nav.Link>
-            <Nav.Link href="#Pads">Pads</Nav.Link>
-          </Nav>
-          <CartWidget />
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        <Navbar NavBar expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">💻 PC Store</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#Monitores">Monitores</Nav.Link>
+              <Nav.Link href="#Teclados">Teclados</Nav.Link>
+              <Nav.Link href="#Pads">Pads</Nav.Link>
+            </Nav>
+            <CartWidget />
+          </Navbar.Collapse>
+        </Container>
+        </Navbar>
     );
 }
 
